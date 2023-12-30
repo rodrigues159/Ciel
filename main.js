f5aa9b7a1a5cb120896694312b983c2150bc2101
rodrigues159 <57396876+rodrigues159@users.noreply.github.com>
2023-12-30 19:56:56 -0300

    adicionada função de privacidade para os canais de voz

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
-const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent } = require('discord.js');
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, Modal, TextInputComponent, MessageSelectMenu  } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -17,14 +17,16 @@ let criadoresDeCanais;
 // Carregar dados dos canais no início
 try {
   const dadosCarregados = JSON.parse(fs.readFileSync('dadosCanais.json', 'utf-8'));
-  criadoresDeCanais = new Map(dadosCarregados.canais);
+  criadoresDeCanais = new Map(dadosCarregados.canais.map(entry => [entry.idchannel, entry.ownerChannel]));
 } catch (error) {
   criadoresDeCanais = new Map();
 }
 
 function salvarDados() {
-  const dados = { canais: Array.from(criadoresDeCanais) };
-  fs.writeFileSync('dadosCanais.json', JSON.stringify(dados, null, 2));
+  const dadosParaSalvar = {
+    canais: Array.from(criadoresDeCanais).map(([idchannel, ownerChannel]) => ({ idchannel, ownerChannel })),
+  };
+  fs.writeFileSync('dadosCanais.json', JSON.stringify(dadosParaSalvar, null, 2), 'utf-8');
 }
 
 // Função para criar a interface de botões
@@ -47,12 +49,76 @@ function criarInterfaceDeBotao() {
         .setCustomId(SetMaxMembers.getCustomId())
         .setLabel('')
         .setEmoji('<:limitMembers:1190116297166553088>')
+        .setStyle('SECONDARY'),
+      new MessageButton()
+        .setCustomId('privacyOptions')
+        .setLabel('')
+        .setEmoji('<:privacyOptions:1190341360428007484>')
         .setStyle('SECONDARY')
       // Adicione mais botões conforme necessário
     );
   // Continuar adicionando rows e botões conforme necessário
 
   return { embeds: [embed], components: [row] };
+}
+
+function lerDadosCanais() {
+  try {
+    const dados = fs.readFileSync('dadosCanais.json', 'utf-8');
+    return JSON.parse(dados).canais;
+  } catch (error) {
+    console.error('Erro ao ler dados dos canais:', error);
+    return [];
+  }
+}
+
+
+function createPrivacySelectMenu() {
+  const row = new MessageActionRow().addComponents(
+    new MessageSelectMenu()
+      .setCustomId('privacySelect')
+      .setPlaceholder('Escolha uma opção de privacidade')
+      .addOptions([
+        {
+          label: 'Trancar',
+          description: 'Somente usuários confiáveis poderão entrar no seu canal de voz',
+          value: 'lock',
+          emoji: '<:lockIcon:1190351996507656222>' 
+        },
+        {
+          label: 'Desbloquear',
+          description: 'Todos poderão entrar no seu canal de voz',
+          value: 'unlock',
+          emoji: '<:unlockIcon:1190352073317957652>'
+        },
+        {
+          label: 'Invisível',
+          description: 'Somente usuários confiáveis poderão visualizar seu canal de voz',
+          value: 'invisible',
+          emoji: '<:invisibleIcon:1190352143840972810>'
+        },
+        {
+          label: 'Visível',
+          description: 'Todos poderão ver seu canal de voz',
+          value: 'visible',
+          emoji: '<:visibleIcon:1190352308052176926>'
+        },
+        {
+          label: 'Fechar bate-papo',
+          description: 'Somente usuários confiáveis poderão enviar mensagens de texto no seu bate-papo',
+          value: 'closeChat',
+          emoji: '<:blockChatIcon:1190352391602708520>'
+        },
+        {
+          label: 'Abrir bate-papo',
+          description: 'Todos poderão enviar mensagens de texto no seu bate-papo',
+          value: 'openChat',
+          emoji: '<:unblockChatIcon:1190352460334764093>'
+        },
+      ]),
+  );
+
+  return row;
 }
 
 client.once('ready', async () => {
@@ -172,83 +238,220 @@ const SetMaxMembers = require('./commands/SetMaxMembers');
 client.on('interactionCreate', async (interaction) => {
   const memberVoiceChannel = interaction.member.voice.channel;
 
-    if (interaction.isButton()) {
-        if (interaction.customId === 'cname') {
-            if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
-                return interaction.reply({ content: 'Você não tem permissão para renomear este canal ou não está em um canal de voz temporário.', ephemeral: true });
-            }
+  if (interaction.isButton()) {
+    if (interaction.customId === 'cname') {
+      if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
+        return interaction.reply({ content: 'Você não tem permissão para editar este canal ou não está em um canal de voz temporário.', ephemeral: true });
+      }
 
-            const modal = new Modal()
-                .setCustomId('renameModal')
-                .setTitle('Renomear Canal de Voz')
-                .addComponents([
-                    new MessageActionRow().addComponents(
-                        new TextInputComponent()
-                            .setCustomId('newChannelName')
-                            .setLabel('Novo nome para o canal de voz')
-                            .setStyle('SHORT')
-                            .setPlaceholder(':Porvalope:')
-                            .setRequired(true),
-                    ),
-                ]);
+      const modal = new Modal()
+        .setCustomId('renameModal')
+        .setTitle('Renomear Canal de Voz')
+        .addComponents([
+          new MessageActionRow().addComponents(
+            new TextInputComponent()
+              .setCustomId('newChannelName')
+              .setLabel('Novo nome para o canal de voz')
+              .setStyle('SHORT')
+              .setPlaceholder(':Porvalope:')
+              .setRequired(true),
+          ),
+        ]);
 
-            await interaction.showModal(modal);
-        } else if (interaction.customId === SetMaxMembers.getCustomId()) {
-          if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
-            return interaction.reply({ content: 'Você não tem permissão para renomear este canal ou não está em um canal de voz temporário.', ephemeral: true });
-          }
+      await interaction.showModal(modal);
+    } else if (interaction.customId === SetMaxMembers.getCustomId()) {
+      if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
+        return interaction.reply({ content: 'Você não tem permissão para editar este canal ou não está em um canal de voz temporário.', ephemeral: true });
+      }
 
-          await interaction.showModal(SetMaxMembers.createModal());
+      await interaction.showModal(SetMaxMembers.createModal());
 
-        }
+    } else if (interaction.customId === 'privacyOptions') {
+      if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
+        return interaction.reply({ content: 'Você não tem permissão para editar este canal ou não está em um canal de voz temporário.', ephemeral: true });
+      }
+      const privacyMenu = createPrivacySelectMenu();
+      await interaction.reply({ content: 'Selecione uma opção de privacidade:', components: [privacyMenu], ephemeral: true });
     }
-  
-    if (interaction.isModalSubmit()) {
-      if (interaction.customId === 'renameModal') {
-        const newChannelName = interaction.fields.getTextInputValue('newChannelName');
-        const memberVoiceChannel = interaction.member.voice.channel;
-  
-        if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
-          return interaction.reply({ content: 'Você não tem permissão para renomear este canal ou não está em um canal de voz temporário.', ephemeral: true });
-        }
-  
-        try {
-          await memberVoiceChannel.setName(newChannelName);
-          await interaction.reply({ content: `O canal foi renomeado para: ${newChannelName}`, ephemeral: true });
-        } catch (error) {
-          console.error('Erro ao renomear o canal:', error);
-          await interaction.reply({ content: 'Houve um erro ao tentar renomear o canal.', ephemeral: true });
-        }
-      } else if (interaction.customId === SetMaxMembers.getCustomId()) {
-        const maxMembers = interaction.fields.getTextInputValue('maxMembers');
-        const memberVoiceChannel = interaction.member.voice.channel;
-      
-        if (!memberVoiceChannel) {
-          return interaction.reply({ content: 'Você precisa estar em um canal de voz para usar este comando.', ephemeral: true });
-        }
-      
-        if (!canaisTemporarios.has(memberVoiceChannel.id)) {
-          return interaction.reply({ content: 'Este comando só pode ser usado em canais de voz temporários.', ephemeral: true });
-        }
-      
-        if (criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
-          return interaction.reply({ content: 'Você não é o proprietário deste canal de voz temporário.', ephemeral: true });
-        }
-      
-        try {
-          const userLimit = parseInt(maxMembers, 10);
-          if (isNaN(userLimit) || userLimit < 0) {
-            return interaction.reply({ content: 'Por favor, forneça um número válido.', ephemeral: true });
+  }
+
+  if (interaction.isSelectMenu() && interaction.customId === 'privacySelect') {
+    // Pega a opção selecionada pelo usuário e aplica a lógica correspondente
+    const selectedOption = interaction.values[0];
+    switch (selectedOption) {
+      case 'lock':
+        // Negar a permissão de conectar para todos os membros
+        await memberVoiceChannel.permissionOverwrites.set([
+          {
+            id: interaction.guild.roles.everyone.id,
+            deny: ['CONNECT']
+          },
+          {
+            id: interaction.member.id, // Garante que o membro que trancou o canal ainda possa se conectar
+            allow: ['CONNECT']
           }
+        ]);
+        // Responder ao usuário que o canal foi trancado
+        await interaction.update({
+          content: 'O canal foi trancado. Ninguém mais pode entrar.',
+          components: [],
+          ephemeral: true
+        });
+        break;
+      case 'unlock':
+          // Permitir a permissão de conectar para todos os membros
+          await memberVoiceChannel.permissionOverwrites.edit(interaction.guild.roles.everyone, { CONNECT: null });
+          await interaction.update({
+            content: 'O canal foi desbloqueado. Todos os usuários podem entrar agora.',
+            components: [],
+            ephemeral: true
+          });
+        break;
+      case 'invisible':
+          // Definir o canal para ser visível apenas pelo proprietário ou por um cargo confiável
+          await memberVoiceChannel.permissionOverwrites.set([
+            {
+              id: interaction.guild.roles.everyone.id,
+              deny: ['VIEW_CHANNEL']
+            },
+            {
+              id: interaction.member.id,
+              allow: ['VIEW_CHANNEL', 'CONNECT']
+            }
+            // Se você tiver um cargo confiável que também deve ver o canal, adicione outra sobrescrita aqui
+          ]);
+          await interaction.update({
+            content: 'O canal agora está invisível para os outros usuários.',
+            components: [],
+            ephemeral: true
+          });
+        break;
+      case 'visible':
+          // Remover permissões específicas e voltar ao padrão do servidor
+          await memberVoiceChannel.permissionOverwrites.edit(interaction.guild.roles.everyone, { VIEW_CHANNEL: null });
+          await interaction.update({
+            content: 'O canal agora está visível para todos os usuários.',
+            components: [],
+            ephemeral: true
+          }); 
+        break;
+      case 'closeChat':
+        const canaisData = lerDadosCanais();
+        const canalInfo = canaisData.find(canal => canal.idchannel === memberVoiceChannel.id);
       
-          await memberVoiceChannel.setUserLimit(userLimit);
-          await interaction.reply({ content: `O limite máximo de membros foi definido para: ${userLimit}`, ephemeral: true });
-        } catch (error) {
-          console.error('Erro ao definir o limite de membros:', error);
-          await interaction.reply({ content: 'Houve um erro ao tentar definir o limite de membros.', ephemeral: true });
+        if (!canalInfo || canalInfo.ownerChannel !== interaction.user.id) {
+          await interaction.update({
+            content: 'Você não é o proprietário deste canal de voz.',
+            components: [],
+            ephemeral: true
+          });
+          break;
         }
+      
+        const textChannel = memberVoiceChannel.guild.channels.cache.get(canalInfo.idchannel);
+      
+        if (textChannel) {
+          await textChannel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SEND_MESSAGES: false });
+          await interaction.update({
+            content: 'O bate-papo foi fechado.',
+            components: [],
+            ephemeral: true
+          });
+        } else {
+          await interaction.update({
+            content: 'Não foi possível encontrar um canal de texto vinculado.',
+            components: [],
+            ephemeral: true
+          });
+        }
+        break;
+      case 'openChat':
+          const canaisData2 = lerDadosCanais();
+          const canalInfo2 = canaisData2.find(canal => canal.idchannel === memberVoiceChannel.id);
+        
+          if (!canalInfo2) {
+            await interaction.update({
+              content: 'Não foi possível encontrar um canal de texto vinculado.',
+              components: [],
+              ephemeral: true
+            });
+            break;
+          }
+        
+          const textChannel2 = memberVoiceChannel.guild.channels.cache.get(canalInfo2.idchannel);
+        
+          if (textChannel2) {
+            await textChannel2.permissionOverwrites.edit(interaction.guild.roles.everyone, { SEND_MESSAGES: null });
+            await interaction.update({
+              content: 'O bate-papo foi aberto. Todos os usuários podem enviar mensagens agora.',
+              components: [],
+              ephemeral: true
+            });
+          } else {
+            await interaction.update({
+              content: 'Não foi possível encontrar um canal de texto vinculado.',
+              components: [],
+              ephemeral: true
+            });
+          }
+          break;
+        
+        // ... outros casos
+        
+      default:
+        await interaction.reply({ content: 'Opção de privacidade não reconhecida.', ephemeral: true });
+
+        // Confirma a ação para o usuário de forma efêmera
+        await interaction.update({ content: `A opção '${selectedOption}' foi aplicada com sucesso.`, components: [], ephemeral: true });
+    }
+  }
+
+  if (interaction.isModalSubmit()) {
+    if (interaction.customId === 'renameModal') {
+      const newChannelName = interaction.fields.getTextInputValue('newChannelName');
+      const memberVoiceChannel = interaction.member.voice.channel;
+
+      if (!memberVoiceChannel || !canaisTemporarios.has(memberVoiceChannel.id) || criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
+        return interaction.reply({ content: 'Você não tem permissão para editar este canal ou não está em um canal de voz temporário.', ephemeral: true });
+      }
+
+      try {
+        await memberVoiceChannel.setName(newChannelName);
+        await interaction.reply({ content: `O canal foi renomeado para: ${newChannelName}`, ephemeral: true });
+      } catch (error) {
+        console.error('Erro ao renomear o canal:', error);
+        await interaction.reply({ content: 'Houve um erro ao tentar renomear o canal.', ephemeral: true });
+      }
+    } else if (interaction.customId === SetMaxMembers.getCustomId()) {
+      const maxMembers = interaction.fields.getTextInputValue('maxMembers');
+      const memberVoiceChannel = interaction.member.voice.channel;
+
+      if (!memberVoiceChannel) {
+        return interaction.reply({ content: 'Você precisa estar em um canal de voz para usar este comando.', ephemeral: true });
+      }
+
+      if (!canaisTemporarios.has(memberVoiceChannel.id)) {
+        return interaction.reply({ content: 'Este comando só pode ser usado em canais de voz temporários.', ephemeral: true });
+      }
+
+      if (criadoresDeCanais.get(memberVoiceChannel.id) !== interaction.user.id) {
+        return interaction.reply({ content: 'Você não é o proprietário deste canal de voz temporário.', ephemeral: true });
+      }
+
+      try {
+        const userLimit = parseInt(maxMembers, 10);
+        if (isNaN(userLimit) || userLimit < 0) {
+          return interaction.reply({ content: 'Por favor, forneça um número válido.', ephemeral: true });
+        }
+
+        await memberVoiceChannel.setUserLimit(userLimit);
+        await interaction.reply({ content: `O limite máximo de membros foi definido para: ${userLimit}`, ephemeral: true });
+      } catch (error) {
+        console.error('Erro ao definir o limite de membros:', error);
+        await interaction.reply({ content: 'Houve um erro ao tentar definir o limite de membros.', ephemeral: true });
       }
     }
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
